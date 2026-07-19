ec80947549406a94b73a39f953edb56035569a51
Lars Kellogg-Stedman <lars@redhat.com>
2026-07-19 16:01:41 -0400

    Manage organization settings

---
+++ organization.tf
@@ -0,0 +1,25 @@
+resource "github_organization_settings" "this" {
+  billing_email                                                = "okrieg@bu.edu"
+  blog                                                         = "https://massopen.cloud/"
+  name                                                         = "Mass Open Cloud"
+  description                                                  = "The MOC Alliance accelerates the pace of discovery and innovation in an open production research-focused cloud"
+  location                                                     = "Boston, MA"
+  has_organization_projects                                    = true
+  has_repository_projects                                      = true
+  default_repository_permission                                = "none"
+  members_can_create_repositories                              = true
+  members_can_create_public_repositories                       = true
+  members_can_create_private_repositories                      = true
+  members_can_create_internal_repositories                     = false
+  members_can_create_pages                                     = true
+  members_can_create_public_pages                              = true
+  members_can_create_private_pages                             = true
+  members_can_fork_private_repositories                        = true
+  web_commit_signoff_required                                  = false
+  advanced_security_enabled_for_new_repositories               = false
+  dependabot_alerts_enabled_for_new_repositories               = false
+  dependabot_security_updates_enabled_for_new_repositories     = false
+  dependency_graph_enabled_for_new_repositories                = false
+  secret_scanning_enabled_for_new_repositories                 = false
+  secret_scanning_push_protection_enabled_for_new_repositories = false
+}
