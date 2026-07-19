f873ca5883d8ebb59300cf0f43d800150ddeb5ac
Lars Kellogg-Stedman <lars@redhat.com>
2026-07-19 14:30:22 -0400

    Manage team membership

--- teams.tf
+++ teams.tf
@@ -1,5 +1,9 @@
 locals {
-  teams = csvdecode(file("${path.module}/teams.csv"))
+  teams      = csvdecode(file("${path.module}/teams.csv"))
+  team_slugs = toset([for f in fileset("${path.module}/team-members", "*.csv") : trimsuffix(f, ".csv")])
+  team_members = {
+    for slug in local.team_slugs : slug => csvdecode(file("${path.module}/team-members/${slug}.csv"))
+  }
 }
 
 resource "github_team" "this" {
@@ -8,4 +12,17 @@ resource "github_team" "this" {
   description    = each.value.description
   privacy        = each.value.privacy
   parent_team_id = each.value.parent != "" ? each.value.parent : null
+}
+
+resource "github_team_members" "this" {
+  for_each  = local.team_members
+  team_slug = each.key
+
+  dynamic "members" {
+    for_each = each.value
+    content {
+      username = members.value.username
+      role     = members.value.role
+    }
+  }
 }
