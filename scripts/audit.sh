2a206a74d73654ab127ec4d8b9b400dafb501140
Lars Kellogg-Stedman <lars@redhat.com>
2026-07-19 13:10:07 -0400

    Manage teams

--- scripts/audit.sh
+++ scripts/audit.sh
@@ -33,6 +33,11 @@ managed_members=$(
   jq '.values.root_module | .. | select(.type? == "github_membership") | .values.username' -r <<<"$state" | sort
 )
 
+log info "finding managed teams"
+managed_teams=$(
+  jq '.values.root_module | .. | select(.type? == "github_team_members") | .index' -r <<<"$state" | sort
+)
+
 log info "finding github repositories"
 github_repositories=$(
   gh api --paginate /orgs/cci-moc/repos | jq '.[]|.name' -r | sort
@@ -43,8 +48,14 @@ github_members=$(
   gh api --paginate /orgs/cci-moc/members | jq '.[]|.login' -r | sort
 )
 
+log info "finding github teams"
+github_teams=$(
+  gh api --paginate /orgs/cci-moc/teams | jq '.[]|.slug' -r | sort
+)
+
 unmanaged_repositories=$(comm -13 <(echo "$managed_repositories") <(echo "$github_repositories"))
 unmanaged_members=$(comm -13 <(echo "$managed_members") <(echo "$github_members"))
+unmanaged_teams=$(comm -13 <(echo "$managed_teams") <(echo "$github_teams"))
 
 exitcode=0
 
@@ -58,6 +69,12 @@ fi
 if [[ -n "$unmanaged_members" ]]; then
   log error "Found unmanaged members:"
   log error "$unmanaged_members"
+  exitcode=1
+fi
+
+if [[ -n "$unmanaged_teams" ]]; then
+  log error "Found unmanaged teams:"
+  log error "$unmanaged_teams"
   exitcode=1
 fi
 
